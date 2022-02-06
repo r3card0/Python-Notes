cb756f9b4d1124458baacc720d7452f3ae545f41
r3card0 <ricardo.rdz@pm.me>
2022-02-06 16:02:58 -0600

    Tectonic plates list

---
+++ Scripts_py/Geosciences/tectonic_plates_list.py
@@ -0,0 +1,12 @@
+# Tectonic plates list. Reference: Essentials of Geology, 13th Edition by Frederick K. Lutgens and Edward J. Tarbuck
+def run():
+    tectonic_plates = ['North-American plate','Eurasian plate','Philipine plate',
+    'Juan de Fuca plate','Cocos plate','Caribbean plate','Nazca plate','South-American plate',
+    'African plate','Pacific plate','Arabian plate','Australian-Indian plate','Scotia plate','Antartic plate']
+    for plate in tectonic_plates:
+        print(plate)
+
+
+
+if __name__ == '__main__':
+    run()
