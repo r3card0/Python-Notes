b9ec41054fd1ae3867632f98ab189077d17ba435
r3card0 <ricardo.rdz@pm.me>
2023-03-13 23:28:08 -0600

    decorator calculates elapsed time of a sum

---
+++ Projects/Python_Professional_Course/decoradores/suma.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def execution_time(func):
+    def wrapper(*args,**kwargs):
+        initial_time = datetime.now()
+        func(*args,**kwargs)
+        final_time = datetime.now()
+        time_elapsed = final_time - initial_time
+        print('Pasaron ' + str(time_elapsed.total_seconds()) + ' segundos')
+    return wrapper
+
+@execution_time
+def suma(a:int,b:int)->int:
+    return a + b
+
+
+print(suma(45,56))
