5dedef0f6ce0840810ccc738e581360a17c28e9a
r3card0 <ricardo.rdz@pm.me>
2023-03-13 23:27:25 -0600

    decorator calculates elapsed time

---
+++ Projects/Python_Professional_Course/decoradores/executiontime.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def execution_time(func):
+    def wrapper():
+        initial_time = datetime.now()
+        func()
+        final_time = datetime.now()
+        time_elapsed = final_time - initial_time
+        print('Pasaron ' + str(time_elapsed.total_seconds()) + ' segundos')
+    return wrapper
+
+@execution_time
+def random_func():
+    for _ in range(1,1000000000):
+        pass
+
+
+random_func()
