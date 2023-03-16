05959a85cded0649968789e394ef92cda948635e
r3card0 <ricardo.rdz@pm.me>
2023-03-16 01:33:14 -0600

    converted to oop

--- Projects/Fun/guess-password.py
+++ Projects/Fun/guess-password.py
@@ -1,12 +1,31 @@
-def ask_for_password():
-    password = str(input("Please! Guess the password: "))
-    while password != 'please':
-        print("This is not the password, please, try again!")
-        password = input("Please! Guess the password: ")
-    if password == 'please':
-        print("Finally!")
-        print("Bye!")
 
+class Guess:
+    def __init__(self) -> None:
+        self.password = str(input('Please! Guess the password: '))
+
+    def ask_password(self):
+        while self.password != 'please':
+            print("This is not the password, please, try again!")
+            self.password = input("Please! Guess the password: ")
+        if self.password == 'please':
+            print("Finally!")
+            print("Bye!")
+
+def run():
+    game = Guess()
+    game.ask_password()
 
 if __name__=='__main__':
-    ask_for_password()
+    run()
+
+
+# def ask_for_password():
+#     password = str(input("Please! Guess the password: "))
+#     while password != 'please':
+#         print("This is not the password, please, try again!")
+#         password = input("Please! Guess the password: ")
+#     if password == 'please':
+#         print("Finally!")
+#         print("Bye!")
+
+
