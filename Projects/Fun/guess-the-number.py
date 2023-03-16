312f66b184a84f7fa25bcaca9eb705cfda3f2061
r3card0 <ricardo.rdz@pm.me>
2023-03-16 02:07:10 -0600

    converted to oop

--- Projects/Fun/guess-the-number.py
+++ Projects/Fun/guess-the-number.py
@@ -1,28 +1,27 @@
-def guess():
-    number = int(input("Please! choose a number: "))
-    while number != 95:
-        if number > 95:
-            print("Your number is greather than my number")
-            number = int(input("Please! choose a number: "))
-        elif number == 95:
-            print("Great! Your number is equal than mine, is 95")        
-        else:
-            print("Your number is less than my number")
-            number = int(input("Please! choose a number: "))
-
-if __name__=="__main__":
-    guess()
-
-
-       
+class GuessNumber:
+    def __init__(self) -> None:
+        self.number = int(input("Please! choose a number: "))
     
-    
+    def guess(self):
+        while self.number != 15:
+            if self.number == 15:
+                print("Great! Your number is equal than mine, is 15")
+                print("You done!")
+            elif self.number > 15:
+                print("Your number is greather than my number")
+                self.number = int(input("Please! choose a number: "))
+            # elif self.number == 15:
+            #     print("Great! Your number is equal than mine, is 15")
+            #     print("You done!")
+            else:
+                print("Your number is less than my number")
+                self.number = int(input("Please! choose a number: "))
 
+def run():
+    game = GuessNumber()
+    game.guess()
 
-
-
-
-
-
+if __name__ == "__main__":
+    run()
 
 
