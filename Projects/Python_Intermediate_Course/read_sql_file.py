bb6deded6235ec7a2a1bd1b6188dfe5efc1fabf2
r3card0 <ricardo.rdz@pm.me>
2023-03-19 16:20:16 -0600

    converted to oop

--- Projects/Python_Intermediate_Course/read_sql_file.py
+++ Projects/Python_Intermediate_Course/read_sql_file.py
@@ -3,11 +3,22 @@
 # 3. use for loop to append into empty list
 # 4. convert list to string
 
-def read_sql_file():
-    sql = []
-    with open('/file/path/script.sql','r',encoding='utf-8') as file:
-        for line in file:
-            sql.append(line)
-        
-        sql_statement = """""".join(sql)
-        return sql_statement
+class Read:
+    def __init__(self,pathfile) -> None:
+        self.pathfile = pathfile
+
+    def read_sql_file(self):
+        self.sql = []
+        with open(self.pathfile,'r',encoding='utf-8') as file:
+            for line in file:
+                self.sql.append(line)
+
+            sql_statement = """""".join(self.sql)
+            return sql_statement
+
+# Instance
+test = Read('/path/file.sql')
+
+print(test.read_sql_file())
+
+
